f4b5e2edb48141cd7d62a4e33f0e5ff2294b863c
spnichols <stephen.p.nichols@gmail.com>
2014-08-30 14:11:19 -0700

    removing TestingJava8 and changed TEstingJava7

--- TestingJava7/src/Something.java
+++ TestingJava7/src/Something.java
@@ -3,7 +3,7 @@ public class Something {
 
 	public static void main(String[] args) {
 		System.out.println("This is to test Creating a project on PC with java 7");
-
+		System.out.println("Thank god this stuff works, stick with Java 7 for now");
 	}
 
 }
