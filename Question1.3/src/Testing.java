4d9d25e8780692b05165c8beb72743be7d337f0c
spnichols <stephen.p.nichols@gmail.com>
2014-09-11 19:10:48 -0700

    interview update

--- Question1.3/src/Testing.java
+++ Question1.3/src/Testing.java
@@ -1,4 +1,30 @@
+/*Design an algorithm and write code to remove the duplicate characters in a string
+without using any additional buffer. NOTE: One or two additional variables are fine.
+An extra copy of the array is not.
+FOLLOW UP
+Write the test cases for this method*/
+
 
 public class Testing {
 
+	public static void main(String args[]){
+		
+	}
+	
+	static String removeDup(String input){
+		if (input == null) return null;
+		String output = input;
+		
+		for(int x = 0; x < output.length(); x++){
+			for(int y = 0; y < output.length(); y++){
+				if(x != y){
+					if(output.charAt(x) == output.charAt(y)){
+						
+					}
+				}
+			}
+		}
+		
+		return input;
+	}
 }
--- Question1.7/src/Testing.java
+++ Question1.7/src/Testing.java
@@ -11,6 +11,7 @@ public class Testing {
 				System.out.print(testingArray[x][y] + " ");
 			}
 			System.out.println("");
+			System.out.println("hahaha");
 		}
 	}
 	
